3ee0cd0a8c9ac4b77b338f2d7e3b96f03ea7598e
Ernesto Gallegos <isnotinfinity@gmail.com>
2026-06-28 19:05:41 -0600

    Añadir workflow para compilar usbliter8 para Pico 2W

--- boards/pico2_w.cmake
+++ boards/pico2_w.cmake
@@ -1,9 +1,6 @@
-# boards/pico2_w.cmake
-# Configuración específica para Raspberry Pi Pico 2W
-
 add_compile_definitions(
     BOARD_NAME="Raspberry Pi Pico 2W"
     PIO_USB_DP_PIN_DEFAULT=12
     LED_SINGLE_COLOR=1
-    LED_PIN=PICO_DEFAULT_LED_PIN
+    LED_PIN=25
 )
